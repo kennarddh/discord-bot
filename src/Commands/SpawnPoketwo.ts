c0ee9c0748ead9d34bbfed92e047bb7ef8e698df
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-06-19 15:47:32 +0700

    feat: Write to poketwo data when spawning

--- src/Commands/SpawnPoketwo.ts
+++ src/Commands/SpawnPoketwo.ts
@@ -3,6 +3,7 @@ import { EmbedBuilder, SlashCommandBuilder } from 'discord.js'
 import { ICommand } from './Types'
 import SendAsOtherPerson from '../Utils/SendAsOtherPerson.js'
 import RandomInt from '../Utils/RandomInt.js'
+import Poketwo from '../Data/Poketwo.js'
 
 const SpawnPoketwo: ICommand = {
 	data: new SlashCommandBuilder()
@@ -17,9 +18,35 @@ const SpawnPoketwo: ICommand = {
 				.setMinValue(1)
 		) as SlashCommandBuilder,
 	async execute(interaction) {
+		if (Poketwo.pendingCatchSpecies)
+			return void interaction.reply({
+				content: 'Error pending catch already spawned',
+				ephemeral: true,
+			})
+
 		const speciesId =
 			(interaction.options as any).getInteger('species-id') ||
 			(RandomInt(1, 1010) as number)
+
+		const pokeApiResultRaw = await fetch(
+			`https://pokeapi.co/api/v2/pokemon/${speciesId}`
+		).catch(() => {
+			interaction.reply({
+				content: 'Error fetching poke api',
+				ephemeral: true,
+			})
+		})
+
+		if (!pokeApiResultRaw) return
+
+		const pokeApiResult = await pokeApiResultRaw.json()
+
+		const name = (pokeApiResult.name as string)
+			.split(' ')
+			.map(
+				part => `${part.slice(0, 1).toUpperCase()}${part.substring(1)}`
+			)
+			.join(' ')
 
 		const imageUrl = `https://server.poketwo.io/image?time=day&species=${speciesId}`
 
@@ -35,7 +62,7 @@ const SpawnPoketwo: ICommand = {
 			)
 			.setImage(imageUrl)
 
-		SendAsOtherPerson(
+		await SendAsOtherPerson(
 			interaction.guild,
 			interaction.channel,
 			poketwoId,
@@ -46,6 +73,8 @@ const SpawnPoketwo: ICommand = {
 				interaction.reply({ content: 'Error', ephemeral: true })
 			}
 		)
+
+		Poketwo.pendingCatchSpecies = { id: speciesId, name }
 	},
 }
 

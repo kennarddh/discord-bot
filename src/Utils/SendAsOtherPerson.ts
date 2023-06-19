8ea5dab757bda2109d2a5dfb08baffda61d74c96
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-06-19 15:15:21 +0700

    feat: Send as other person util

---
+++ src/Utils/SendAsOtherPerson.ts
@@ -0,0 +1,32 @@
+import { Channel, Guild, WebhookMessageCreateOptions, TextChannel } from 'discord.js'
+
+const SendAsOtherPerson = async (
+	guild: Guild,
+	channel: Channel,
+	userId: string,
+	message: WebhookMessageCreateOptions,
+	onError: (error: Error) => void
+) => {
+	const member = await guild.members.fetch(userId)
+
+	const name = member.displayName || member.nickname
+
+	const webhook = await (channel as TextChannel)
+		.createWebhook({
+			name: name,
+			avatar: member.displayAvatarURL(),
+		})
+		.catch(onError)
+
+	if (!webhook) return
+
+	try {
+		await webhook.send(message)
+
+		await webhook.delete()
+	} catch (error) {
+		onError(error)
+	}
+}
+
+export default SendAsOtherPerson

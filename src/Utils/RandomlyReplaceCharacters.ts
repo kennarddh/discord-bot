043c718b3a04877134c2d34e76ca9ec7971b1eb7
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-06-19 17:36:15 +0700

    refactor: Randomly replace characters

--- src/Utils/RandomlyReplaceCharacters.ts
+++ src/Utils/RandomlyReplaceCharacters.ts
@@ -1,9 +1,17 @@
 import RandomBoolean from './RandomBoolean.js'
 
-const RandomlyReplaceCharacters = (str: string, trueChance: number) =>
-	str
-		.split('')
-		.map(char => (RandomBoolean(trueChance) ? char : '_'))
-		.join('')
+const RandomlyReplaceCharacters = (str: string, trueChance: number) => {
+	let newStr = ''
+
+	for (const char of str) {
+		if (RandomBoolean(trueChance)) {
+			newStr += char
+		} else {
+			newStr += '_'
+		}
+	}
+
+	return newStr
+}
 
 export default RandomlyReplaceCharacters
